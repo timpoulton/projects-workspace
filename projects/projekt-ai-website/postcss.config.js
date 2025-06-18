cf141bd12af2c0aec64422c99ea08ffc06ac33c4
Tim Poulton <root@tpserver.local>
2025-06-18 04:59:40 +0000

    fix: use named purgeCSSPlugin export in postcss.config.js

--- projects/projekt-ai-website/postcss.config.js
+++ projects/projekt-ai-website/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
+    require('@fullhuman/postcss-purgecss').purgeCSSPlugin({
       content: ['./**/*.html'],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
     }),
